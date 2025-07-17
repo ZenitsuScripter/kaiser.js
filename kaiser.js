71ad8b45628c25a20ccae1562ff70905027a7e06
sh.kirito <147661025+ZenitsuScripter@users.noreply.github.com>
2025-07-17 19:10:46 -0300

    Create kaiser.js

---
+++ kaiser.js
@@ -0,0 +1,13 @@
+export function initKaiser(player) {
+  player.characterName = "Kaiser";
+  player.isKaiser = true;
+}
+
+export function renderKaiser(ctx, player) {
+  if (!player.isKaiser) return;
+
+  ctx.fillStyle = "white";
+  ctx.font = "16px Arial";
+  ctx.textAlign = "center";
+  ctx.fillText(player.characterName, player.x, player.y - 40);
+}
