a3814f8e7eb9991aa960c54226c1513c036c1248
sh.kirito <147661025+ZenitsuScripter@users.noreply.github.com>
2025-07-17 21:19:41 -0300

    Update kaiser.js

--- kaiser.js
+++ kaiser.js
@@ -1,1 +1,2 @@
-09876543wsdfghjkmnbvcdftyuifkem84uehdurnejdfokmflplkmKJHBV NJKIugfr*&$*&$#@#$%*98&$%*IJHBVGHNBVCDSERTYUYTRSXS@#$%*()PLMNBVCDERTYUJHBVCXSSW@!@#$%*()_PLKMNBVCDTYJNBV
+const senha = "09876543wsdfghjkmnbvcdftyuifkem84uehdurnejdfokmflplkmKJHBV NJKIugfr*&$*&$#@#$%*98&$%*IJHBVGHNBVCDSERTYUYTRSXS@#$%*()PLMNBVCDERTYUJHBVCXSSW@!@#$%*()_PLKMNBVCDTYJNBV
+";
